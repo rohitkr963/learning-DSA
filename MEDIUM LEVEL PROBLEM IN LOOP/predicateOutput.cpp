27b9e7bb6b3baca540871a5679df8fc51f8d9883
rohitkr963 <nk7565331@gmail.com>
2024-03-13 21:05:16 +0530

    predicate output

---
+++ MEDIUM LEVEL PROBLEM IN LOOP/predicateOutput.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int j;
+    while (j <= 10) {
+        cout << j << endl;
+        j = j + 1;
+    }
+    return 0;
+}
+
+
+
+//  here is something missing 
+//  we have to give the value of j 
+//  without giving the value of j it's nothing print
