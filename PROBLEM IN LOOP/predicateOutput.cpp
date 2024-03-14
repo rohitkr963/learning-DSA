eb66736a99d96ad9ad27e90b2e85a6341f24ae29
rohitkr963 <nk7565331@gmail.com>
2024-03-14 12:45:26 +0530

    predicate output

---
+++ PROBLEM IN LOOP/predicateOutput.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    while ('a' < 'b')
+        cout << "malayalam is a palindrome" << endl;
+    return 0;
+}
+
+//It print infinite because the condition is not true
