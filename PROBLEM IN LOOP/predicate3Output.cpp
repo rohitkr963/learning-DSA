71c84653847c6d5a56a80cd665342146c75e29ed
rohitkr963 <nk7565331@gmail.com>
2024-03-14 12:51:07 +0530

    predicate 3 output

---
+++ PROBLEM IN LOOP/predicate3Output.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+while ('1' < '2')
+cout << "In while loop" << endl;
+}
+
+
+// it print infinit because the condition the while is not true
