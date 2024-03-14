4f5ae9706dd7c6012904affad6816cdb69c956a1
rohitkr963 <nk7565331@gmail.com>
2024-03-14 12:35:58 +0530

    prime or not

---
+++ PROBLEM IN LOOP/checkPrimeOrNot.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, i, count = 0;
+    cout << "Enter any number n: "; 
+    cin>>n;
+    
+    for (i = 1; i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+           count++;
+        }
+    }
+    if (count == 2)
+    {
+       cout << "it is a Prime number" << endl;
+    }
+    else
+    {
+         cout << "it is not a Prime number" << endl; 
+    }
+    return 0;    
+}
