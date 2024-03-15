1b189468078ccceebc42ae1bf552afdd188956cc
rohitkr963 <nk7565331@gmail.com>
2024-03-15 16:01:03 +0530

    star triangle

---
+++ PATTERN PRINTING/starTriangle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+   
+   int n,i,j;
+ 
+
+   for(i=1; i<=4; i++)
+   {
+    for(j=1; j<=i; j++)
+    {
+    cout<<"*";
+   }
+   cout<<endl;
+   }
+    return 0;
+}
