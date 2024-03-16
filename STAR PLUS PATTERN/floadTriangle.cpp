38d51f4a5b52a1cb8f2343d484c6678d0d33d9b8
rohitkr963 <nk7565331@gmail.com>
2024-03-16 20:14:05 +0530

    fload triangle

--- STAR PLUS PATTERN/floadTriangle.cpp
+++ STAR PLUS PATTERN/floadTriangle.cpp
@@ -2,13 +2,14 @@
 using namespace std;
 int main() {
    
-   int n,i,j;
+   int n,i,j,number=1;
 
    for(i=1; i<=4; i++)
    {
     for(j=1; j<=i; j++)
     {
-        cout<<i;
+        cout<<number<<" ";
+        number++;
    }
    cout<<endl;
    }
