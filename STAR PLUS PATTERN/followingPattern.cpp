8e468ca9520a50ecd54be7d9ee33aaa330e3740f
rohitkr963 <nk7565331@gmail.com>
2024-03-16 15:16:42 +0530

    following pattern

---
+++ STAR PLUS PATTERN/followingPattern.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+   
+   int n,i,j;
+   cout<<"enter a number";
+   cin>>n;
+
+   for(i=1; i<=4; i++)
+   {
+    for(j=1; j<=4; j++)
+    {
+        cout<<i;
+   }
+   cout<<endl;
+   }
+    return 0;
+}
