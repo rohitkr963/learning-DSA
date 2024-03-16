d679b7a52a5b67fc6a12d611743f4194e244c3ac
rohitkr963 <nk7565331@gmail.com>
2024-03-16 15:12:28 +0530

    number triangle flip

---
+++ STAR PLUS PATTERN/floadTriangle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+   
+   int n,i,j;
+
+   for(i=1; i<=4; i++)
+   {
+    for(j=1; j<=i; j++)
+    {
+        cout<<i;
+   }
+   cout<<endl;
+   }
+    return 0;
+}
