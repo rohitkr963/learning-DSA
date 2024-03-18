ac80bc1cc013edfacc69ad3a602e9b0189731a9b
rohitkr963 <nk7565331@gmail.com>
2024-03-18 21:48:21 +0530

    binary triangle

---
+++ PATTERN PRINTING/binaryTriangle.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+   
+   int n,i,j;
+ 
+
+   for(i=1; i<=4; i++)
+   {
+    for(j=1; j<=i; j++)
+    {
+    if((i+j)%2==0)
+    {
+        cout<<"1";
+    }
+    else{
+        cout<<"0";
+    }
+   }
+   cout<<endl;
+   }
+    return 0;
+}
