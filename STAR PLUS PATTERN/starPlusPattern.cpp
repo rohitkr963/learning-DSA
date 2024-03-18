9ac0a5307c9d7445cdbb7beef2707ce3a0465aa8
rohitkr963 <nk7565331@gmail.com>
2024-03-18 22:00:19 +0530

    star plus pattern

---
+++ STAR PLUS PATTERN/starPlusPattern.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() {
+   
+   int n,i,j;
+   cout<<"enter a number";
+   cin>>n;
+   int mid=n/2+1;
+
+   for(i=1; i<=n; i++)
+   {
+    for(j=1; j<=n; j++)
+    {
+        if(i==mid||j==mid)
+        {
+            cout<<"*";
+        }
+        else 
+        {
+            cout<<" ";
+        }
+   }
+   cout<<endl;
+   }
+    return 0;
+}
