c111693e8fb7d6b6e00d189043eb84e87bc6dedd
rohitkr963 <nk7565331@gmail.com>
2024-03-31 10:56:43 +0530

    special pattern

---
+++ STAR PLUS PATTERN/pattern.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+for(int i=1; i<=5; i++)
+{
+    for(int j=1; j<=5; j++)
+    {
+        if(i==1&&j<5||j==1&&i<5)
+        {
+            cout<<"*";
+        }
+        else{
+            cout<<" ";
+        }
+        if(j==5&&i<=5||i==5&&j<=5)
+        {
+            cout<<"*";
+        }
+        else{
+            cout<<" ";
+        }
+    }
+cout<<endl;
+}
+return 0;
+
+}
