2949dfb7e169eb629eb31ded4963ecc9e5018870
rohitkr963 <nk7565331@gmail.com>
2024-03-31 23:07:26 +0530

    maximum value in array

---
+++ ARRAY/maximumValueInArray.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,i;
+    int arr[5]={5,8,3,9,6};
+    int max=arr[0];
+   
+
+    for(i=0; i<5; i++)
+    {
+        if(arr[i]>max)
+        {
+            max=arr[i];
+        }
+    }
+
+    cout<<max;
+}
