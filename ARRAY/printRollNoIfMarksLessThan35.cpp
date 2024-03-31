ccc5d7e0e6f110d6a2a2d7f976745d62185e28de
rohitkr963 <nk7565331@gmail.com>
2024-03-31 22:36:17 +0530

    print index if marks is less than 35

--- ARRAY/printRollNoIfMarksLessThan35.cpp
+++ ARRAY/printRollNoIfMarksLessThan35.cpp
@@ -3,12 +3,13 @@ using namespace std;
 
 int main() {
     int marks[5] = {50, 20, 70, 40, 30}; //example marks array
-    int n = sizeof(marks)/sizeof(marks[0]); //finding the size of array
-
-    for (int i = 0; i < n; i++) { //loop through all elements
-        if (marks[i] < 35) { //check if marks is less than 35
-            cout << "Student with roll number " << i << " has marks below 35." << endl;
+    for(int i=0; i<5; i++)
+    {
+        if(marks[i]<35)
+        {
+            cout<<i<<endl;
         }
     }
+    //cout<<marks[i];
     return 0;
 }
