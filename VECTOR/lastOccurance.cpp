07c729d3c138e67aa06496ca5da5a469c7f5bc77
rohitkr963 <nk7565331@gmail.com>
2024-04-04 15:53:42 +0530

    last occurance

---
+++ VECTOR/lastOccurance.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    vector<int> v(6);
+
+    for(int i=0; i<6; i++)
+    {
+        cin>>v[i];
+    }
+
+    int x;
+    cout<<"enter a number";
+    cin>>x;
+
+    int occurance=-1;
+    for(int i=0; i<6; i++)
+    {
+        if(v[i]==x)
+        {
+            occurance=i;
+        }
+    }
+    cout<<occurance;
+}
