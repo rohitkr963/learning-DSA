e7a7d0cbe77bad1037c589224cc067138b18fde3
rohitkr963 <nk7565331@gmail.com>
2024-04-06 15:05:45 +0530

    copy from one array to another

--- VECTOR/copyFromOneArrayToAnother.cpp
+++ VECTOR/copyFromOneArrayToAnother.cpp
@@ -1,30 +1,35 @@
 #include<iostream>
-using namespace std;
+using namespace std; 
 
 int main()
 {
-    int arr1[100], arr2[100], i, n;
-    cout << "Enter the size of the array: ";
-    cin >> n;
+    int arr[5];
+    int n;
+    cout<<"enter a size of array";
+    cin>>n;
 
-    cout << "Enter elements for the array: ";
-    for(i = 0; i < n; i++)
+    for(int i=0; i<n; i++)
     {
-        cin >> arr1[i];
+        cin>>arr[i];
+    }
+    int j=0;
+
+    // take another array for store the value of previos array 
+    int ansarr[5];
+
+    // copy array in the reverse order
+    for(int i=n-1; i>=0; i--)
+    {
+        ansarr[j++]=arr[i];
     }
 
-    // Copying elements to the second array in reverse order
-    for(i = n-1; i >= 0; i--)
+ 
+    // print our ansarray
+    for(int i=0; i<n; i++)
     {
-        arr2[i] = arr1[n-i-1];
-    }
-
-    // Printing the contents of the second array
-    cout << "Elements of array2: ";
-    for(i = 0; i < n; i++)
-    {
-        cout << arr2[i] << " ";
+        cout<<ansarr[i]<<" ";
     }
 
     return 0;
+
 }
