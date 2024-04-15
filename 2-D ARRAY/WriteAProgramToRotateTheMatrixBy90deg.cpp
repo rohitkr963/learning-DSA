09d86c19091709ccb2e9e9403d40179ae7cc3620
rohitkr963 <nk7565331@gmail.com>
2024-04-15 10:08:39 +0530

    Rotate matrix by 90 deg

---
+++ 2-D ARRAY/WriteAProgramToRotateTheMatrixBy90deg.cpp
@@ -0,0 +1,44 @@
+
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 10;
+
+int main() {
+    int n;
+
+    cout << "Enter the number of rows/columns of the square matrix: ";
+    cin >> n;
+
+    int mat[MAX][MAX];
+    cout << "Enter the elements of the matrix:" << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> mat[i][j];
+        }
+    }
+
+    int temp[MAX][MAX];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            temp[j][n - 1 - i] = mat[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            mat[i][j] = temp[i][j];
+        }
+    }
+
+    cout << "Matrix after rotating by 90 degrees clockwise:" << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
