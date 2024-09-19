53274bf07e00683d8bfba7aff958463a3d9b0ca1
rohitkr963 <nk7565331@gmail.com>
2024-09-19 20:44:50 +0530

    c++

--- RECURSION - 1/factorial.cpp
+++ RECURSION - 1/factorial.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 int fact(int n){
+
+    // base case .......... means where we have to use terminating condition
     if(n == 1 || n==0) return 1;
     return n*fact(n-1);
 }
